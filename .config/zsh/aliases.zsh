9f8f0aff9c5903fcdc46dd772b8a5cb65ceef407
Alexander Orlov <eadenink@gmail.com>
2025-01-24 01:07:02 +0400

    feat: Add alias for ls command to use eza instead

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -5,5 +5,7 @@ alias v="nvim"
 # Find file via fzf and open it in nvim
 alias vf='nvim $(fzf --preview="bat --color=always --style=numbers --line-range=:500 {}")'
 
+alias ls="eza --long --group-directories-first --git --color=always --icons=always --no-time --no-user --no-filesize --no-permissions"
+
 alias pgstart="brew services start postgresql@16"
 alias pgstop="brew services stop postgresql@16"
