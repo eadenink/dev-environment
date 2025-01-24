31dac3587fcc0382586f93a2ec5eefc2cd09554c
Alexander Orlov <eadenink@gmail.com>
2025-01-24 22:23:00 +0400

    feat: Update lualine config

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -1,7 +1,28 @@
 return {
 	{
 		"nvim-lualine/lualine.nvim",
-		dependencies = { "nvim-tree/nvim-web-devicons" },
-		opts = {},
+		dependencies = { "nvim-tree/nvim-web-devicons", "f-person/git-blame.nvim" },
+		config = function()
+			local gitblame = require("gitblame")
+
+			vim.g.gitblame_display_virtual_text = 0
+
+			require("lualine").setup({
+				options = {
+					component_separators = { left = "|", right = "" },
+					section_separators = { left = "", right = "" },
+				},
+				sections = {
+					lualine_a = { "mode" },
+					lualine_b = { "branch", "diff" },
+					lualine_c = {
+						{ gitblame.get_current_blame_text, cond = gitblame.is_blame_text_available },
+					},
+					lualine_x = { "encoding", "filename" },
+					lualine_y = {},
+					lualine_z = { "location" },
+				},
+			})
+		end,
 	},
 }
