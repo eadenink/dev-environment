34f269e4e78286315a81eefe961a807338192f76
Alexander Orlov <eadenink@gmail.com>
2025-01-24 22:22:43 +0400

    feat: Set breakpoint sign for dap

--- .config/nvim/lua/plugins/debugging/dap.lua
+++ .config/nvim/lua/plugins/debugging/dap.lua
@@ -4,6 +4,8 @@ return {
 		config = function()
 			local dap = require("dap")
 
+			vim.fn.sign_define("DapBreakpoint", { text = "🛑", texthl = "", linehl = "", numhl = "" })
+
 			local keymap = vim.keymap
 
 			keymap.set("n", "<leader>dt", dap.toggle_breakpoint, {})
