4c59f8d823bef26df70a58125c6708de114d8243
Alexander Orlov <eadenink@gmail.com>
2025-01-27 02:08:21 +0400

    feat: Impersonate zsh paths

--- .config/zsh/completions.zsh
+++ .config/zsh/completions.zsh
@@ -1,4 +1,4 @@
 # Bun
-[ -s "/Users/alexorlov/.bun/_bun" ] && source "/Users/alexorlov/.bun/_bun"
+[ -s "$HOME/.bun/_bun" ] && source "$HOME/.bun/_bun"
 # Node verions manager
 [ -s "$NVM_DIR/bash_completion" ] && source "$NVM_DIR/bash_completion"
--- .config/zsh/path.zsh
+++ .config/zsh/path.zsh
@@ -1,11 +1,11 @@
 # Meteor
-export PATH=/Users/alexorlov/.meteor:$PATH
+export PATH="$HOME/.meteor:$PATH"
 
 # Postgresql
 export PATH="/usr/local/opt/postgresql@16/bin:$PATH"
 
 # SST
-export PATH=/Users/alexorlov/.sst/bin:$PATH
+export PATH="$HOME/.sst/bin:$PATH"
 
 # Bun
 export PATH="$HOME/.bun/bin:$PATH"
@@ -14,14 +14,12 @@ export PATH="$HOME/.bun/bin:$PATH"
 export PATH="/usr/local/opt/python@3.13/libexec/bin:$PATH"
 
 # Golang
-export GOPATH=$HOME/go
-export PATH=$PATH:$GOPATH/bin
+export PATH="$HOME/go/bin:$PATH"
 
 # Android tools
-export ANDROID_HOME=$HOME/Library/Android/sdk
-export PATH=$PATH:$ANDROID_HOME/emulator
-export PATH=$PATH:$ANDROID_HOME/platform-tools
+export ANDROID_HOME="$HOME/Library/Android/sdk"
+export PATH="$ANDROID_HOME/emulator:$PATH"
+export PATH="$ANDROID_HOME/platform-tools:$PATH"
 
 # Java
-export JAVA_HOME=$(/usr/libexec/java_home)
-export PATH=$JAVA_HOME/bin:$PATH
+export PATH="/usr/libexec/java_home/bin:$PATH"
