1c0c684bddb11fb6c2e760600025cdbd11105719
Alexander Orlov <eadenink@gmail.com>
2025-01-29 09:09:46 +0400

    feat: Remap explorer and update plugins

--- .config/nvim/lua/eadenink/remap.lua
+++ .config/nvim/lua/eadenink/remap.lua
@@ -1,6 +1,6 @@
 local keymap = vim.keymap
 
-keymap.set("n", "<leader>pv", vim.cmd.Ex)
+keymap.set("n", "<leader>e", vim.cmd.Ex)
 
 keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 keymap.set("v", "K", ":m '<-2<CR>gv=gv")
