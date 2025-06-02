6dc76c8296ac9ab5b79559a62a3fcac3c75cec8b
Alexander Orlov <eadenink@gmail.com>
2025-06-02 09:55:50 +0300

    feat: Add paste from clipboard nvim keymap

--- .config/nvim/lua/eadenink/remap.lua
+++ .config/nvim/lua/eadenink/remap.lua
@@ -6,6 +6,7 @@ keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
 keymap.set("x", "<leader>p", '"_dP')
+keymap.set({ "n", "v" }, "<leader>P", [["+p]])
 
 keymap.set({ "n", "v" }, "<leader>y", [["+y]])
 keymap.set("n", "<leader>Y", [["+Y]])
