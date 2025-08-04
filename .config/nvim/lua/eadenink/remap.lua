8ea029c2a7bf2c3974f20a87653ad7bf26dd0bc5
Alexander Orlov <eadenink@gmail.com>
2025-08-04 19:55:02 +0300

    feat: Add keymap to make file executable

--- .config/nvim/lua/eadenink/remap.lua
+++ .config/nvim/lua/eadenink/remap.lua
@@ -1,12 +1,21 @@
 local keymap = vim.keymap
 
+-- Open explorer
 keymap.set("n", "<leader>e", vim.cmd.Ex)
 
+-- Move around selected lines vertically
 keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
+-- Paste without overriding register
 keymap.set("x", "<leader>p", '"_dP')
+
+-- Paste from clipboard
 keymap.set({ "n", "v" }, "<leader>P", [["+p]])
 
+-- Copy to clipboard
 keymap.set({ "n", "v" }, "<leader>y", [["+y]])
 keymap.set("n", "<leader>Y", [["+Y]])
+
+-- Make file executable
+vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
