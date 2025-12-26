20aa8a25bb5785e8c2332f60cfabf5fe88c8684e
Alexander Orlov <eadenink@gmail.com>
2025-12-26 11:29:51 +0300

    feat: Add osx setup script

---
+++ scripts/setup/osx.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+source "~/scripts/utils/ask_yes_no.sh"
+
+# Install brew if not installed
+if ! command -v brew &> /dev/null; then
+    echo "Installing Homebrew..."
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+else
+    echo "Homebrew already installed"
+fi
+
+echo "Installing uv and python..."
+curl -LsSf https://astral.sh/uv/install.sh | sh
+uv python install --default
+
+echo "Installing cli tools..."
+brew install neovim tmux tpm fzf grep zsh ghostty go lazygit lazydocker eza zoxide docker kubernetes-cli postgresql starship gh
+echo "Installing gui apps..."
+brew install --cask obsidian postman gcloud-cli font-jetbrains-mono-nerd-font google-drive
+
+echo "Starting services..."
+brew services start docker
+brew services start postgresql
+
+if ask_yes_no "Do you want to login to GitHub CLI?" "y"; then
+    gh auth login
+else
+    echo "Skipping GitHub CLI login."
+fi
